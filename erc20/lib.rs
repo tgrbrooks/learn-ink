3c34bb13223b29598c2b3499e5b6faef30ce1cf9
tgrbrooks <thomas.brooks@appliedblockchain.com>
2023-01-30 13:31:51 +0000

    Add transfer and event

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -9,6 +9,23 @@ mod erc20 {
         Mapping,
     };
 
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
     /// Create storage for a simple ERC-20 contract.
     #[ink(storage)]
     #[derive(SpreadAllocate)]
@@ -34,6 +51,11 @@ mod erc20 {
             let caller = Self::env().caller();
             self.balances.insert(&caller, &initial_supply);
             self.total_supply = initial_supply;
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: initial_supply,
+            });
         }
 
         /// Returns the total token supply.
@@ -46,6 +68,39 @@ mod erc20 {
         #[ink(message)]
         pub fn balance_of(&self, owner: AccountId) -> Balance {
             self.balances.get(owner).unwrap_or_default()
+        }
+
+        #[inline]
+        fn balance_of_impl(&self, owner: &AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.transfer_from_to(&from, &to, value)
+        }
+
+        fn transfer_from_to(
+            &mut self,
+            from: &AccountId,
+            to: &AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let from_balance = self.balance_of_impl(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance)
+            }
+
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of_impl(to);
+            self.balances.insert(to, &(to_balance + value));
+            self.env().emit_event(Transfer {
+                from: Some(*from),
+                to: Some(*to),
+                value,
+            });
+            Ok(())
         }
     }
 
@@ -67,6 +122,17 @@ mod erc20 {
             assert_eq!(contract.total_supply(), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.total_supply(), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
+            assert_eq!(contract.transfer(AccountId::from([0x0; 32]), 10), Ok(()));
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 90);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
         }
     }
 }
